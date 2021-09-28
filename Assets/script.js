a5dd4c4335ddcda3e0869284ac7419fd2e51151d
Kaleidosport <lambert.ant14@gmail.com>
2021-09-28 09:51:44 +0200

    First shot at JS, slight edit to index.html

---
+++ Assets/script.js
@@ -0,0 +1,4 @@
+// First step: creating an array containing all the words the user will have to find
+
+const WORDS = ["Orgrimmar", "Darnassus", "Stormwind", "Undercity", "Silvermoon", "Ironforge", "Gilneas", "Thunderbluff",
+ "Kezan", "Exodar", "Dalaran", "Shattrath", "Suramar", "Tiragarde", "Zuldazar", "Kalimdor", "Northrend", "Draenor"]
