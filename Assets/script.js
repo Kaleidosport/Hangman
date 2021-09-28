64b31d526b15cf5080c2ffb1545b942b8e770d5b
Kaleidosport <lambert.ant14@gmail.com>
2021-09-28 10:46:18 +0200

    Further edited script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,16 @@
 // First step: creating an array containing all the words the user will have to find
+const WORDS = ["Azeroth", "Orgrimmar", "Darnassus", "Stormwind", "Undercity", "Silvermoon", "Ironforge",
+ "Gilneas", "Thunderbluff", "Kezan", "Exodar", "Dalaran", "Shattrath", "Suramar", "Tiragarde", "Zuldazar",
+  "Kalimdor", "Northrend", "Zandalar", "Draenor", "Nagrand", "Argus", "Shadowlands", "Oribos"]
 
-const WORDS = ["Orgrimmar", "Darnassus", "Stormwind", "Undercity", "Silvermoon", "Ironforge", "Gilneas", "Thunderbluff",
- "Kezan", "Exodar", "Dalaran", "Shattrath", "Suramar", "Tiragarde", "Zuldazar", "Kalimdor", "Northrend", "Draenor"]
+// Second step: picking a word at random
+let wordPicker = WORDS[Math.floor(Math.random() * WORDS.length)]
+wordPicker = wordPicker.toUpperCase() // Ensuring uppercase is deemed acceptable as well
+
+// Third step: setting up an array depending on how many letters picked word contains
+let lettersInWord = []
+for (let i = 0; i < wordPicker.length; i++) {
+    lettersInWord[i] = "_" // Filling in the array with underscores
+}
+
+let remainingLetters = wordPicker.length // Monitoring how many letters are yet to be guessed
