a2d377bd55b07ba6b0101f1b4ffca7d343558a72
Kaleidosport <lambert.ant14@gmail.com>
2021-09-29 12:34:54 +0200

    First gaming items added

--- Assets/script.js
+++ Assets/script.js
@@ -3,11 +3,15 @@ const WORDS = ["Azeroth", "Orgrimmar", "Darnassus", "Stormwind", "Undercity", "S
  "Gilneas", "Thunderbluff", "Kezan", "Exodar", "Dalaran", "Shattrath", "Suramar", "Tiragarde", "Zuldazar",
   "Kalimdor", "Northrend", "Zandalar", "Draenor", "Nagrand", "Argus", "Shadowlands", "Oribos"]
 
-// Second step: picking a word at random
+// Second step: creating an alphabet-devoted array
+const ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// Third step: picking a word at random
 let wordPicker = WORDS[Math.floor(Math.random() * WORDS.length)]
 wordPicker = wordPicker.toUpperCase() // Ensuring uppercase is deemed acceptable as well
 
-// Third step: setting up an array depending on how many letters picked word contains
+// Fourth step: setting up an array depending on how many letters picked word contains
 let lettersInWord = []
 for (let i = 0; i < wordPicker.length; i++) {
     lettersInWord[i] = "_" // Filling in the array with underscores
@@ -17,5 +21,11 @@ let remainingLetters = wordPicker.length // Monitoring how many letters are yet 
 
 // Game on!
 document.getElementById("play").addEventListener("click", () => {
-    document.getElementById("play").style.visibility = "hidden";
+    document.getElementById("play").style.visibility = "hidden"; // Hiding PLAY button once clicked
+    let guessedLetters = []
+    guessedLetters = wordPicker.split("").forEach(element => /[a-z]/i.test(element) ? 
+    guessedLetters.push("_") : guessedLetters.push(element)) // Filling an array with guessed letters
+    /* Creating the game interface to be displayed */
+    document.getElementById("game-interface").innerHTML = 
+    `<p>Try to guess the following word</p> <span class="size-and-spacing">${lettersInWord.join(" ")}</span>` // Displaying word to find as underscores
 })
